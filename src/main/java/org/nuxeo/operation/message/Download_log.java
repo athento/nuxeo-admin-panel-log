fff9723d87c1e132847cbeafa4bc03b5d59011d8
MiguelGlezMart <miguelglezmarti@gmail.com>
2015-09-28 15:20:54 +0200

    Initial commit

---
+++ src/main/java/org/nuxeo/operation/message/Download_log.java
@@ -0,0 +1,55 @@
+package org.nuxeo.operation.message;
+
+import java.io.File;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationChain;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.jsf.operations.DownloadFile;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
+import org.nuxeo.runtime.api.Framework;
+
+@Name("download_log")
+@Scope(ScopeType.CONVERSATION)
+public class Download_log {
+
+	private String input;
+
+	public void submit() {
+		// handle form submission
+		/* output = "Caught message: " + input; */
+		String pathLog;
+		if (input.equals("")) {
+			pathLog = "/var/log/nuxeo/server.log";
+		} else {
+			pathLog = "/var/log/nuxeo/server.log." + input;
+		}
+		File logFile = new File(pathLog);
+		if (logFile.exists()) {
+			Blob blob = new FileBlob(logFile);
+			OperationChain chain = new OperationChain("DownloadServerLogFile");
+			chain.add(DownloadFile.ID);
+			OperationContext ctx = new OperationContext();
+			ctx.setInput(blob);
+			try {
+				Framework.getLocalService(AutomationService.class).run(ctx,
+						chain);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			input = "";
+		}
+	}
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+}
